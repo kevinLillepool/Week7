6c5b5ca12a151dbf49ddbf19b149210d55729989
Kevin Lillepool <kevin.lillepool@tktk.ee>
2024-10-28 23:10:00 +0200

    Week 7

---
+++ CalculatorWithFunctions/Program.cs
@@ -0,0 +1,41 @@
+﻿int a = 10;
+int b = 2;
+
+Add(a, b);
+Add(10, 100);
+Add(56583, 658686);
+
+Multiply(a, b);
+Multiply(10, 100);
+Multiply(56583, 658686);
+
+Subtract(a, b);
+Subtract(10, 100);
+Subtract(56583, 658686);
+
+Divide(a, b);
+Divide(100, 10);
+Divide(658686, 56583);
+
+static void Add(int x, int y)
+{
+    Console.WriteLine($"{x} + {y} = {x + y}");
+}
+
+static void Multiply(int x, int y)
+{
+    Console.WriteLine($"{x} * {y} = {x * y}");
+}
+
+static void Subtract(int x, int y)
+{
+    Console.WriteLine($"{x} - {y} = {x - y}");
+}
+
+static void Divide(int x, int y)
+{
+    if (y != 0)
+    {
+        Console.WriteLine($"{x} / {y} = {x / y}");
+    }
+}  
---
+++ DisplayArrayElementsFunction/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] fruitBasket = { "apples", "bananas", "oranges" };
+string[] pets = { "cats", "dogs", "fish", "hamster" };
+string[] characters = { "Harry", "Ron" };
+int[] numbers = { 1, 2, 3, 4, 5, 6 };
+
+PrintArrayElements(fruitBasket);
+PrintArrayElements(pets);
+PrintArrayElements(characters);
+
+static void PrintArrayElements(string[] someArray) 
+{
+    for (int i = 0; i < someArray.Length; i++)
+    {
+        Console.WriteLine($"{i + 1}. {someArray[i]}");
+    }
+}
+
